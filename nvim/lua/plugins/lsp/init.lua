a6b0fde5fa008f339cc2160e73a0612771c15bc2
Valeryi Savich <Relrin78@gmail.com>
2024-02-27 20:08:29 +0100

    Fixed cmp_nvim_lsp call after breaking changes

--- nvim/lua/plugins/lsp/init.lua
+++ nvim/lua/plugins/lsp/init.lua
@@ -80,7 +80,10 @@ return {
             vim.diagnostic.config(opts.diagnostics)
   
             local servers = opts.servers
-            local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
+            local capabilities = vim.tbl_deep_extend("force",
+              vim.lsp.protocol.make_client_capabilities(),
+              require('cmp_nvim_lsp').default_capabilities()
+            )
   
             local function setup(server)
                 local server_opts = vim.tbl_deep_extend("force", {
